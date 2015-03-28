28a2676249c9ef5ee555734b7c396fb7b9694d30
voidrank <voidrank@gmail.com>
2015-03-28 23:40:55 +0800

    initial commit

---
+++ src/ASA/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib import admin
+from django.conf.urls.static import static
+
+urlpatterns = patterns(
+    '',
+    # Examples:
+    # url(r'^$', 'ASA.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    # grapelli
+    url(r'^grappelli/', include('grappelli.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'admin/login.html'}),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+        {'template_name': 'logged_out.html'}),
+    url(r'', include('website.urls')),
+    url(r'', include('video_cms')),
+) + \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
